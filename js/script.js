0eb6dc4088ec88281e4ee58a7036cce838e4a811
Filippo Rossi <rossifilippo2000@gmail.com>
2021-11-11 16:27:24 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -21,3 +21,43 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+let pointer = 0;
+
+for (let i = 0; i < items.length; i++) {
+    if(i == pointer){
+        document.querySelector(".slider").innerHTML +=
+        `
+        <div class="item active">
+            <img src="${items[i]}" alt="${title[i]}">
+        </div>
+        <div class="title active">
+            <h1>${title[i]}</h1>
+            <h2>${text[i]}</h2>
+        </div>
+        `
+        document.querySelector(".cards-box").innerHTML +=
+        `
+        <div class="card active">
+            <img src="${items[i]}" alt="${title[i]}">
+        </div>
+        `
+    } else {
+        document.querySelector(".slider").innerHTML +=
+        `
+        <div class="item">
+            <img src="${items[i]}" alt="${title[i]}">
+        </div>
+        <div class="title">
+            <h1>${title[i]}</h1>
+            <h2>${text[i]}</h2>
+        </div>
+        `
+        document.querySelector(".cards-box").innerHTML +=
+        `
+        <div class="card">
+            <img src="${items[i]}" alt="${title[i]}">
+        </div>
+        `
+    }
+}
